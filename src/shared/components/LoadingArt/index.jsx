39e7352c6c63ab575c6541e0d42afc4dd8a42f57
Vinicius Rosa <viniciusrosarafaeldeoliveira@gmail.com>
2021-11-10 01:01:51 -0300

    improove loading

--- src/shared/components/LoadingArt/index.jsx
+++ src/shared/components/LoadingArt/index.jsx
@@ -9,10 +9,10 @@ import styles from './styles.module.css';
 function LoadingArt() {
     return <div className={styles.container}>
         <div className={styles.moto}>
-            <Image src={Moto} width={200} height={160} alt="Moto" />
+            <Image src={Moto} width={200} height={160} alt="Moto" quality={40} />
         </div>
         <figure className={styles.fumaca}>
-            <Image src={Fumaca} width={60} height={60} alt="Fumaça" />
+            <Image src={Fumaca} width={60} height={60} alt="Fumaça" quality={40} />
         </figure>
     </div>;
 }
