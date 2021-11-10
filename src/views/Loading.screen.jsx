82f206758a7c6fcd8762dcdcda7fcaf8e7e44474
Vinicius Rosa <viniciusrosarafaeldeoliveira@gmail.com>
2021-11-10 00:50:10 -0300

    remove scroll on loading

--- src/views/Loading.screen.jsx
+++ src/views/Loading.screen.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import LoadingArt from 'shared/components/LoadingArt'
 
 import styles from './Loading.module.css';
 
 function LoadingScreen() {
+    useEffect(() => {
+        document.body.style.overflowY = "hidden"
+
+        return () => {
+            document.body.style.overflowY = "unset"
+        }
+    }, [])
+
     return <div className={styles.container}>
         <LoadingArt />
         <h1 className={styles.title}>Carregando...</h1>
