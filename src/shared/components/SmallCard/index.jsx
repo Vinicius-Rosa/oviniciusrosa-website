bd3573ee77f5f598cfa844a9191a4a2045d91f8a
Vinicius Rosa <viniciusrosarafaeldeoliveira@gmail.com>
2021-11-09 23:21:20 -0300

    tips

--- src/shared/components/SmallCard/index.jsx
+++ src/shared/components/SmallCard/index.jsx
@@ -4,8 +4,11 @@ import Image from 'next/image';
 
 import styles from './styles.module.css';
 
-function SmallCard({ imageUrl, imageDesc }) {
+function SmallCard({ imageUrl, imageDesc, infoRender }) {
     return <div className={styles.cardContainer}>
+        <div className={styles.baloon}>
+            {infoRender}
+        </div>
         <Image src={imageUrl} alt={imageDesc} className="techimage" />
     </div>;
 }
--- src/shared/components/Techs/index.jsx
+++ src/shared/components/Techs/index.jsx
@@ -16,45 +16,70 @@ import styles from './styles.module.css';
 const techsArray = [
     {
         imageUrl: ReactLogo,
-        imageDesc: "Logo React"
+        imageDesc: "Logo React",
+        infoRender: <span>
+            O <strong>React</strong> é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.
+        </span>
     },
     {
         imageUrl: VueLogo,
-        imageDesc: "Logo Vue"
+        imageDesc: "Logo Vue",
+        infoRender: <span>
+            <strong>Vue.js</strong> é um framework JavaScript de código-aberto, focado no desenvolvimento de interfaces de usuário e aplicativos de página única.
+        </span>
     },
     {
         imageUrl: NodeLogo,
-        imageDesc: "Logo Node"
+        imageDesc: "Logo Node",
+        infoRender: <span>
+            Baseado no interpretador V8 do Google, o <strong>Node.js</strong> permite a execução de códigos JavaScript fora de um navegador web.
+        </span>
     },
     {
         imageUrl: PHPLogo,
-        imageDesc: "Logo PHP"
+        imageDesc: "Logo PHP",
+        infoRender: <span>
+            <strong>PHP</strong> é uma linguagem interpretada livre, usada originalmente apenas para o desenvolvimento de aplicações web.
+        </span>
     },
     {
         imageUrl: FigmaLogo,
-        imageDesc: "Logo Figma"
+        imageDesc: "Logo Figma",
+        infoRender: <span>
+            <strong>Figma</strong> é um editor gráfico de vetor e prototipagem de projetos de design baseado principalmente no navegador web.
+        </span>
     },
     {
         imageUrl: AdobeXDLogo,
-        imageDesc: "Logo AdobeXD"
+        imageDesc: "Logo AdobeXD",
+        infoRender: <span>
+            Presente no pacote Adobe, o <strong>XD</strong> é uma ferramenta de design de experiência do usuário baseada em vetores para aplicativos da web e aplicativos móveis.
+        </span>
     },
     {
         imageUrl: PhotoshopLogo,
-        imageDesc: "Logo Photoshop"
+        imageDesc: "Logo Photoshop",
+        infoRender: <span>
+            Presente no pacote Adobe, <strong>Photoshop</strong> é líder no mercado dos editores de imagem profissionais.
+        </span>
     },
     {
         imageUrl: NextJSLogo,
-        imageDesc: "Logo NextJS"
+        imageDesc: "Logo NextJS",
+        infoRender: <span>
+            <strong>Next.js</strong> é uma estrutura da web de desenvolvimento front-end React de código aberto criada por Vercel.
+        </span>
     },
 ]
 
 function Techs() {
     const techsListRender = useMemo(() => techsArray.map(
-        ({ imageUrl, imageDesc }, i) => (
+        ({ imageUrl, imageDesc, infoRender }, i) => (
             <SmallCard
                 key={i}
                 imageUrl={imageUrl}
                 imageDesc={imageDesc}
+                infoRender={infoRender}
             />
         )
     ), [])
