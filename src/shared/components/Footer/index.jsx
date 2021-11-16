710f9c28a634262ee0a15574750420058b195500
Vinicius Rosa <viniciusrosarafaeldeoliveira@gmail.com>
2021-11-16 13:32:27 -0300

    link to me

--- src/shared/components/Footer/index.jsx
+++ src/shared/components/Footer/index.jsx
@@ -4,7 +4,13 @@ import styles from './styles.module.css';
 
 function Footer() {
     return <footer className={styles.footer}>
-        <h1>Desenvolvido com {"<3"} por <a href="#">Vinicius Rosa</a> </h1>
+        <h1>Desenvolvido com {"<3"} por{" "}
+            <a target="_blank" rel="noreferrer"
+                href="https://github.com/Vinicius-Rosa"
+            >
+                Vinicius Rosa
+            </a>
+        </h1>
         <p>Todos os direitos reservados </p>
     </footer>;
 }
